ce1c734507788bd4e834ea766db676419540f514
LowellUfot <lowellufot@gmail.com>
2022-12-04 08:47:39 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 8):
-    for j in range(0, 9):
-        if (j > i) and (i != 8) and (j != 9):
+    for j in range(0, 10):
+        if (j > i):
             print("{0}{1}, ".format(i, j), end="")
 print("89")
