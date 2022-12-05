112a217bb09dab9ea508ae4b00eae928c2078510
LowellUfot <lowellufot@gmail.com>
2022-12-05 12:00:40 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,8 @@ def multiple_returns(sentence):
     length = len(sentence)
     firstLetter = sentence[0]
 
+    if length == 0:
+        return (length, None)
+
     tupleReturn = length, firstLetter
     return tupleReturn
