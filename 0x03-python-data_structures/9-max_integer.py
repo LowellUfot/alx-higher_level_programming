914f37498111d22c7c7d98703379afcd968af104
LowellUfot <lowellufot@gmail.com>
2022-12-05 12:44:17 -0800

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,6 @@ def max_integer(my_list=[]):
         return None
 
     for i in range(len(my_list)):
-        if my_list[i] >= maxNum:
+        if my_list[i] > maxNum:
             maxNum = my_list[i]
     return maxNum
