2ce8f8fa24840111e0745dde8bab66c1584ea7cf
LowellUfot <lowellufot@gmail.com>
2022-12-07 14:30:52 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,9 +2,10 @@
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary is not None:
         new_dict = a_dictionary
-        new_dic[key] = value
-        if key in a_dictionary == True:
-            a_dictionary[key] = value
-        else:
-            a_dictionary[key] = value
+        new_dict[key] = value
+       # if key in a_dictionary == True:
+       #    a_dictionary[key] = value
+       #     nds
+       # else:
+       #     a_dictionary[key] = value
         return new_dict
