56a6372b4f75b09a9747a67b85d5be8b0944777c
LowellUfot <lowellufot@gmail.com>
2022-12-07 14:46:57 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        person, score = None
+        person = None
+        score = None
         for i in a_dictionary.keys():
             if score is None or a_dictionary[i] > score:
                 score = a_dictionary[i]
