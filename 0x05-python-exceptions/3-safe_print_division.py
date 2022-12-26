1f71baea0c5dea7f4cf3dde80ecfe10ff38a6ccd
LowellUfot <lowellufot@gmail.com>
2022-12-26 08:13:41 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,6 +3,7 @@ def safe_print_division(a, b):
     try:
         res = a/b
     except (ZeroDivisionError, TypeError):
-        return None
+        res = None
     finally:
         print("Inside result: {}".format(res))
+    return res
