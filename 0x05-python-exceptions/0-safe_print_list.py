390e549fdb3c164bd083e1478a2a4aa7b20310c8
LowellUfot <lowellufot@gmail.com>
2022-12-26 07:04:55 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i], end=""))
             count +=
-        except(TypeError, ValueError, IndexError, LookupError):
+        except(TypeError, ValueError):
             pass
+        except(IndexError, LookupError):
+            break
     print()
     return count
