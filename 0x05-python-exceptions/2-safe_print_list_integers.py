a8045657610296a4035a4dbcf2a7befb2e2c7d60
LowellUfot <lowellufot@gmail.com>
2022-12-26 07:33:13 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        except (ValueError, TypeError, IndexError, LookupError):
+        except (ValueError, TypeError):
             pass
+        except (IndexError, LookupError):
+            break
     print()
     return count
