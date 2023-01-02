a8b66ab3e0aeccfb609486d8ca9b95def57f62db
LowellUfot <lowellufot@gmail.com>
2023-01-02 09:52:44 -0800

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Rectangle module definition
+This module defines a `Rectangle` class
+"""
+
+
+class Rectangle:
+    '''This defines the Rectangle Object
+    It's empty for now
+    '''
+    pass
