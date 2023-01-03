f412ea5b541dbc0ce8dd08ccaafc230b9f78a1fe
LowellUfot <lowellufot@gmail.com>
2023-01-03 03:46:30 -0800

    #!/usr/bin/python3
    Rectangle = __import__(8-rectangle).Rectangle
    
    my_rectangle_1 = Rectangle(8, 4)
    
    my_rectangle_2 = Rectangle(2, 3)
    
    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
    
        print("my_rectangle_1 is bigger or equal to my_rectangle_2")
    
    else:
    
        print("my_rectangle_2 is bigger than my_rectangle_1")
    
    my_rectangle_2.width = 10
    
    my_rectangle_2.height = 5
    
    if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
    
        print("my_rectangle_1 is bigger or equal to my_rectangle_2")
    
    else:
    
        print("my_rectangle_2 is bigger than my_rectangle_1")

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -103,7 +103,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect2.area():
-            return rect_1.area()
-        else rect_1.area() < rect_2.area():
-            return rect_2.area()
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
