98d8a31ae8294ac7bf7b570f174b7a849c7074d1
LowellUfot <lowellufot@gmail.com>
2023-01-03 02:06:08 -0800

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -25,12 +25,12 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         """defines deleting the Rectangle object"""
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
