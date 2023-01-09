f63902abf63b092de5279d4ff83d241f91675e16
LowellUfot <lowellufot@gmail.com>
2023-01-09 13:06:37 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module defines the lookup function
+"""
+
+def lookup(obj):
+    """Function that returns list of available attributes
+        and method of an object
+        Args:
+            obj (object): python object
+        Return:
+            list of attributes an method in obj
+    """
+    return dir(obj)
