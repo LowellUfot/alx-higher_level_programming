eaad1d1be7c9bc83006f873564b4e01f9338bfb2
LowellUfot <lowellufot@gmail.com>
2023-03-15 00:23:39 -0700

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+
+const argv = process.argv;
+
+const myNum = +argv[2];
+// print process.argv
+if (isNaN(myNum)) {
+  console.log('Not a number');
+} else if (typeof (myNum) === "number") {
+  console.log(`My number: ${myNum}`);
+} else {
+  console.log('Not a number');
+}
