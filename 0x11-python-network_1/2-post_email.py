72a52dba7dcacebe19ac62911a867fe62255c962
LowellUfot <lowellufot@gmail.com>
2023-03-31 14:23:54 -0700

    script to POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of
+the response (decoded in utf-8).
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+
+    email = urllib.parse.urlencode({'email': sys.argv[2]})
+    email = email.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], email)
+    with urllib.request.urlopen(req) as resp:
+        html = resp.read()
+        print('{}'.format(html.decode('utf-8')))
