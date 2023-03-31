e07c75b4d23762cd834792fb94f0cdd2b55e39ed
LowellUfot <lowellufot@gmail.com>
2023-03-31 13:08:29 -0700

    indentation fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Script to fetch URL"""
+"""Script to fetch URL: https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as resp:
-    html = resp.read()
-    print('Body response: \n\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
+if __name__ == "__main__":
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as resp:
+        html = resp.read()
+        print('Body response: \n\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
