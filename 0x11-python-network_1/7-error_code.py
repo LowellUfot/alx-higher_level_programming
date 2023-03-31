0d23043a7f4f82b96b2ff309ed9555edd297ea96
LowellUfot <lowellufot@gmail.com>
2023-03-31 16:06:26 -0700

    handling errors using requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+"""
+import requests
+import requests.exceptions
+import sys
+
+
+if __name__ == "__main__":
+
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print('Error Code: {}'.format(r.status_code))
+    else:
+        print('{}'.format(r.text))
