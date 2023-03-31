bab74e800380dc9ec93cf2a1d0ad692ac30dc927
LowellUfot <lowellufot@gmail.com>
2023-03-31 16:09:44 -0700

    indentation fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,5 @@ import sys
 
 if __name__ == "__main__":
 
-    email = {'email': sys.argv[2]}
-    req = requests.post(sys.argv[1], email)
-    print('{}'.format(r.text))
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print("{}".format(r.text))
