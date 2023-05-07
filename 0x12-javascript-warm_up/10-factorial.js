74feb737cf37576e8e26aa0efdd477c979878210
LowellUfot <lowellufot@gmail.com>
2023-05-07 02:58:48 -0700

    factorial script

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const process = require('process');
+const argv = process.argv;
+
+if (isNaN(argv[2])) {
+  console.log(1);
+} else {
+  console.log(factorial(parseInt(argv[2])));
+}
+
+function factorial (num) {
+  if (num == 0 || num == 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
