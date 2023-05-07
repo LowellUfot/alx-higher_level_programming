7d1941d70cc160790752060ed69a0d8296e5b0b9
LowellUfot <lowellufot@gmail.com>
2023-05-07 04:54:14 -0700

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const process = require('process');
+const argv = process.argv.slice(2);
+
+console.log(secondBiggest(argv));
+
+function secondBiggest(arr) {
+  if (arr.length <= 1) {
+    console.log(0);
+    return;
+  }
+
+  let biggest = Number.MIN_SAFE_INTEGER;
+  let secondBiggest = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = parseInt(arr[i]);
+    if (num > biggest) {
+      secondBiggest = biggest;
+      biggest = num;
+    } else if (num > secondBiggest && num < biggest) {
+      secondBiggest = num;
+    }
+  }
+
+  return secondBiggest;
+}
