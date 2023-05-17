2be3357dea62513f7a9de40e89f333438f9808a9
LowellUfot <lowellufot@gmail.com>
2023-05-16 23:59:42 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,16 @@
 const SquareClass = require('./5-square');
 class Square extends SquareClass {
   constructor (size) {
-    super(size, size);
+    super(size);
+    this.size = size;
   }
 
   charPrint (c) {
     for (let i = 0; i < this.size; i++) {
       let res = '';
       for (let j = 0; j < this.size; j++) {
-        if (typeof (c) !== 'undefined') {
-          res += 'c';
+        if (c !== undefined) {
+          res += c;
         } else {
           res += 'X';
         }
